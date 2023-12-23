ab8c26649cd1b87f4639ab175fdf20a4cd0aad15
Aditya <adityasah712@gmail.com>
2023-12-23 19:27:43 +0530

    Minimal changes like rename of files and recreating environment

--- my_app.py
+++ my_app.py
@@ -9,6 +9,37 @@ app = Flask(__name__)
 # Load the model (Assuming the model is in the same directory as this script)
 model = joblib.load('dark_pattern_model.joblib')
 
+
+
+
+
+
+
+
+
+    # Prediction function
+def predict_dark_pattern(text):
+    prediction = model.predict([text])
+    return "Dark Pattern" if prediction[0] == 1 else "Not Dark Pattern"
+
+
+
+
+# Define the predict endpoint
+@app.route('/predict', methods=['POST'])
+def predict():
+    json_input = request.json
+    url = json_input['url']
+    text, error = get_text_from_url(url)
+    if error is not None:
+        return jsonify({'error': error}), 500
+    if text:
+        prediction = predict_dark_pattern(text)
+        return jsonify({'prediction': prediction})
+    # else:
+    #     return jsonify({'error': 'Unable to extract text from the provided URL.'}), 500
+    
+
 # Function to extract text from URL
 def get_text_from_url(url):
     headers = {
@@ -25,42 +56,8 @@ def get_text_from_url(url):
     except requests.RequestException as e:
         return None, f"An error occurred: {e}"
 
-# def get_text_from_url(url):
-#     try:
-#         # Get the webpage content
-#         response = requests.get(url)
-#         response.raise_for_status()  # Raise an error on a failed request
-#         # Parse the content using Beautiful Soup
-#         soup = BeautifulSoup(response.text, 'html.parser')
-#         # Extract the text from the webpage
-#         text = soup.get_text(separator=' ')
-#         return text
-#     except requests.RequestException as e:
-#         print(f"An error occurred: {e}")
-#         return ""
 
 
-
-# Prediction function
-def predict_dark_pattern(text):
-    prediction = model.predict([text])
-    return "Dark Pattern" if prediction[0] == 1 else "Not Dark Pattern"
-
-# Define the predict endpoint
-@app.route('/predict', methods=['POST'])
-def predict():
-    json_input = request.json
-    url = json_input['url']
-    text, error = get_text_from_url(url)
-    if error is not None:
-        return jsonify({'error': error}), 500
-    if text:
-        prediction = predict_dark_pattern(text)
-        return jsonify({'prediction': prediction})
-    # else:
-    #     return jsonify({'error': 'Unable to extract text from the provided URL.'}), 500
-    
-    
 
 # Run the Flask application
 if __name__ == '__main__':
